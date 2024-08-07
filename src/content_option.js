53ecbe0cb9b58cae84d18af0efd0d14869027efb
Pradip-Gadal <pradip.gada.ds369@gmail.com>
2024-08-07 17:04:58 +0545

    message

--- src/content_option.js
+++ src/content_option.js
@@ -11,7 +11,7 @@ const introdata = {
         second: "I teach cool",
         third: "I love tech. much",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
+    description: "I’m Pradip Gadal experianced Teacher _ Physics and Mathematics,currently pursuing Master in Physics",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
 };
 
